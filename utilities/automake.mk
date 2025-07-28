almusil/ovn

commit 77a24fc1f86746f53d64e90d307daf80bd3e7eb8

utilities/docker: Fix up container build.
Most of the steps were inaccurate.  Instead, use latest Ubuntu, use
OVS from the submodule inside the OVN repo.

Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>

utilities/docker/Makefile
@@ -1,5 +1,5 @@
-#export OVN_BRANCH=master
-#export OVN_VERSION=2.12
+#export OVN_BRANCH=main
+#export OVN_VERSION=24.03.90
 #export DISTRO=debian
 #export GITHUB_SRC=https://github.com/ovn-org/ovn.git
 #export DOCKER_REPO=ovn-org/ovn

utilities/docker/debian/Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:16.04
+FROM ubuntu:22.04
 MAINTAINER "Aliasgar Ginwala" <aginwala@ebay.com>
 
 ARG OVN_BRANCH

utilities/docker/debian/build.sh
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 OVN_BRANCH=$1
 GITHUB_SRC=$2
 

utilities/docker/install_ovn.sh
@@ -12,29 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 OVN_BRANCH=$1
 GITHUB_SRC=$2
 
-# get ovs source always from master as its needed as dependency
-mkdir /build; cd /build
-git clone --depth 1 -b master https://github.com/openvswitch/ovs.git
-cd ovs;
-mkdir _gcc;
-
-# build and install
-./boot.sh
-cd _gcc
-../configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
---enable-ssl
-cd ..; make -C _gcc install; cd ..
-
-
-# get ovn source
+# Get ovn source.
 git clone --depth 1 -b $OVN_BRANCH $GITHUB_SRC
 cd ovn
 
-# build and install
+# Get OVS submodule, build and install OVS.
+git submodule update --init
+cd ovs
 ./boot.sh
 ./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
---enable-ssl --with-ovs-source=/build/ovs/ --with-ovs-build=/build/ovs/_gcc
-make -j8; make install
+--enable-ssl
+make -j8 install
+cd ..
+
+# Build and install OVN.
+./boot.sh
+./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
+--enable-ssl
+make -j8 install

commit 874eb8f472725149eaed4ceaa9aff8cadca86610

utilities: Remove the docker directory.
The docker directory seems to be unmaintained for a while. The last
update happened in 2020 with one exception in 2024 [0]. As stated in
the commit:

"Most of the steps were inaccurate.  Instead, use latest Ubuntu, use
OVS from the submodule inside the OVN repo."

Which indicates that the tooling wasn't used for a while, remove
it completely as there are other ways how to set up ovn inside
container.

[0] Fixes: 77a24fc1f867 ("utilities/docker: Fix up container build.")
Signed-off-by: Ales Musil <amusil@redhat.com>

utilities/automake.mk
@@ -44,17 +44,7 @@ EXTRA_DIST += \
     utilities/containers/py-requirements.txt \
     utilities/containers/prepare.sh \
     utilities/containers/fedora/Dockerfile \
-    utilities/containers/ubuntu/Dockerfile \
-    utilities/docker/Makefile \
-    utilities/docker/start-ovn \
-    utilities/docker/ovn_default_nb_port \
-    utilities/docker/ovn_default_sb_port \
-    utilities/docker/ovn_default_northd_host \
-    utilities/docker/debian/Dockerfile \
-    utilities/docker/debian/build.sh \
-    utilities/docker/rhel/Dockerfile \
-    utilities/docker/rhel/build.sh \
-    utilities/docker/install_ovn.sh
+    utilities/containers/ubuntu/Dockerfile
 
 CLEANFILES += \
     utilities/ovn-ctl.8 \

utilities/docker/Makefile
@@ -1,20 +1,0 @@
-#export OVN_BRANCH=main
-#export OVN_VERSION=24.03.90
-#export DISTRO=debian
-#export GITHUB_SRC=https://github.com/ovn-org/ovn.git
-#export DOCKER_REPO=ovn-org/ovn
-
-# Example:
-#   make build
-#   make push
-
-REPO = ${DOCKER_REPO}
-tag = ${OVN_VERSION}_${DISTRO}_${OVN_BRANCH}
-
-build: ;docker build -t ${REPO}:${tag} --build-arg DISTRO=${DISTRO} \
---build-arg OVN_BRANCH=${OVN_BRANCH} \
---build-arg GITHUB_SRC=${GITHUB_SRC} -f ${DISTRO}/Dockerfile .
-
-.PHONY: build
-
-push: ;docker push ${REPO}:${tag}

utilities/docker/debian/Dockerfile
@@ -1,20 +1,0 @@
-FROM ubuntu:22.04
-MAINTAINER "Aliasgar Ginwala" <aginwala@ebay.com>
-
-ARG OVN_BRANCH
-ARG GITHUB_SRC
-ARG DISTRO
-
-copy $DISTRO/build.sh /build.sh
-copy install_ovn.sh /install_ovn.sh
-RUN /build.sh $OVN_BRANCH $GITHUB_SRC
-
-COPY ovn_default_nb_port /etc/ovn/ovn_default_nb_port
-COPY ovn_default_sb_port /etc/ovn/ovn_default_sb_port
-COPY ovn_default_northd_host /etc/ovn/ovn_default_northd_host
-
-COPY start-ovn /bin/start-ovn
-VOLUME ["/var/log/openvswitch", \
-"/var/lib/openvswitch", "/var/run/openvswitch", "/etc/openvswitch", \
-"/var/log/ovn", "/var/lib/ovn", "/var/run/ovn", "/etc/ovn"]
-ENTRYPOINT ["start-ovn"]

utilities/docker/debian/build.sh
@@ -1,35 +1,0 @@
-#!/bin/sh
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set -e
-
-OVN_BRANCH=$1
-GITHUB_SRC=$2
-
-# Install deps
-build_deps="apt-utils libelf-dev build-essential libssl-dev python3 \
-wget gdb autoconf libtool git automake bzip2 debhelper \
-dh-autoreconf openssl"
-
-apt-get update
-apt-get install -y ${build_deps}
-
-./install_ovn.sh $OVN_BRANCH $GITHUB_SRC
-
-# remove deps to make the container light weight.
-apt-get remove --purge -y ${build_deps}
-apt-get autoremove -y --purge
-cd ..; rm -rf ovn; rm -rf ovs
-basic_utils="vim kmod net-tools uuid-runtime iproute2"
-apt-get install -y ${basic_utils}

utilities/docker/install_ovn.sh
@@ -1,37 +1,0 @@
-#!/bin/sh
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set -e
-
-OVN_BRANCH=$1
-GITHUB_SRC=$2
-
-# Get ovn source.
-git clone --depth 1 -b $OVN_BRANCH $GITHUB_SRC
-cd ovn
-
-# Get OVS submodule, build and install OVS.
-git submodule update --init
-cd ovs
-./boot.sh
-./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
---enable-ssl
-make -j8 install
-cd ..
-
-# Build and install OVN.
-./boot.sh
-./configure --localstatedir="/var" --sysconfdir="/etc" --prefix="/usr" \
---enable-ssl
-make -j8 install

utilities/docker/ovn_default_nb_port
@@ -1,1 +1,0 @@
-nb_db_port=6641

utilities/docker/ovn_default_northd_host
@@ -1,1 +1,0 @@
-northd_host=127.0.0.1

utilities/docker/ovn_default_sb_port
@@ -1,1 +1,0 @@
-sb_db_port=6642

utilities/docker/rhel/Dockerfile
@@ -1,20 +1,0 @@
-FROM centos:7
-MAINTAINER "Aliasgar Ginwala" <aginwala@ebay.com>
-
-ARG OVN_BRANCH
-ARG GITHUB_SRC
-ARG DISTRO
-
-copy $DISTRO/build.sh /build.sh
-copy install_ovn.sh /install_ovn.sh
-RUN /build.sh $OVN_BRANCH $GITHUB_SRC
-
-COPY ovn_default_nb_port /etc/ovn/ovn_default_nb_port
-COPY ovn_default_sb_port /etc/ovn/ovn_default_sb_port
-COPY ovn_default_northd_host /etc/ovn/ovn_default_northd_host
-
-COPY start-ovn /bin/start-ovn
-VOLUME ["/var/log/openvswitch", \
-"/var/lib/openvswitch", "/var/run/openvswitch", "/etc/openvswitch", \
-"/var/log/ovn", "/var/lib/ovn", "/var/run/ovn", "/etc/ovn"]
-ENTRYPOINT ["start-ovn"]

utilities/docker/rhel/build.sh
@@ -1,37 +1,0 @@
-#!/bin/sh
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-OVN_BRANCH=$1
-GITHUB_SRC=$2
-
-# Install deps
-build_deps="rpm-build yum-utils yum-builddep automake autoconf openssl-devel \
-epel-release python3 gdb libtool git bzip2 perl-core zlib-devel openssl git \
-libtool"
-
-yum update -y
-yum install @'Development Tools'  ${build_deps} -y
-
-./install_ovn.sh $OVN_BRANCH $GITHUB_SRC
-
-# remove unused packages to make the container light weight.
-for i in $(package-cleanup --leaves --all);
-    do yum remove -y $i; yum autoremove -y;
-done
-yum remove ${build_deps} -y
-cd ..; rm -rf ovs; rm -rf ovn
-
-# Install basic utils
-basic_utils="vim-minimal.x86_64 net-tools.x86_64 uuid.x86_64 iproute.x86_64"
-yum install -y ${basic_utils}

utilities/docker/start-ovn
@@ -1,91 +1,0 @@
-#!/bin/bash
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-case $1 in
-        "ovn-nb") /usr/share/ovn/scripts/ovn-ctl run_nb_ovsdb
-        ;;
-        "ovn-sb") /usr/share/ovn/scripts/ovn-ctl run_sb_ovsdb
-        ;;
-        "ovn-northd") ovn-northd --pidfile \
-                      --ovnnb-db="unix:/var/run/ovn/ovnnb_db.sock" \
-                      --ovnsb-db="unix:/var/run/ovn/ovnsb_db.sock" \
-                      --log-file=/var/log/ovn/ovn-northd.log
-        ;;
-        "ovn-northd-cluster") ovn-northd --pidfile \
-                              --ovnnb-db=$OVN_NB_DB \
-                              --ovnsb-db=$OVN_SB_DB \
-                              --log-file=/var/log/ovn/ovn-northd.log
-        ;;
-        "ovn-nb-tcp") source /etc/ovn/ovn_default_nb_port
-                      /usr/share/ovn/scripts/ovn-ctl start_ovsdb
-                      ovn-nbctl set-connection ptcp:$nb_db_port
-                      /usr/share/ovn/scripts/ovn-ctl stop_ovsdb
-                      /usr/share/ovn/scripts/ovn-ctl run_nb_ovsdb
-        ;;
-        "ovn-nb-cluster-create") /usr/share/ovn/scripts/ovn-ctl \
-                                 --db-nb-addr=$host_ip \
-                                 --db-nb-cluster-local-addr=$host_ip \
-                                 start_nb_ovsdb
-                                 ovn-nbctl set-connection ptcp:$nb_db_port
-                                 /usr/share/ovn/scripts/ovn-ctl stop_nb_ovsdb
-                                 /usr/share/ovn/scripts/ovn-ctl \
-                                 --db-nb-addr=$host_ip \
-                                 --db-nb-cluster-local-addr=$host_ip \
-                                 run_nb_ovsdb
-        ;;
-        "ovn-nb-cluster-join") /usr/share/ovn/scripts/ovn-ctl \
-                               --db-nb-addr=$host_ip \
-                               --db-nb-cluster-local-addr=$host_ip \
-                               --db-nb-cluster-remote-addr=$remote_host \
-                               run_nb_ovsdb
-        ;;
-        "ovn-sb-tcp") source /etc/ovn/ovn_default_sb_port
-                      /usr/share/ovn/scripts/ovn-ctl start_ovsdb
-                      ovn-sbctl set-connection ptcp:$sb_db_port
-                      /usr/share/ovn/scripts/ovn-ctl stop_ovsdb
-                      /usr/share/ovn/scripts/ovn-ctl run_sb_ovsdb
-        ;;
-        "ovn-northd-tcp") source /etc/ovn/ovn_default_northd_host
-                          source /etc/ovn/ovn_default_nb_port
-                          source /etc/ovn/ovn_default_sb_port
-                          ovn-northd --pidfile \
-                          --ovnnb-db="tcp:$northd_host:$nb_db_port" \
-                          --ovnsb-db="tcp:$northd_host:$sb_db_port" \
-                          --log-file=/var/log/ovn/ovn-northd.log
-        ;;
-        "ovn-sb-cluster-create") /usr/share/ovn/scripts/ovn-ctl \
-                                 --db-sb-addr=$host_ip \
-                                 --db-sb-cluster-local-addr=$host_ip \
-                                 start_sb_ovsdb
-                                 ovn-sbctl set-connection ptcp:$sb_db_port
-                                 /usr/share/ovn/scripts/ovn-ctl stop_sb_ovsdb
-                                 /usr/share/ovn/scripts/ovn-ctl \
-                                 --db-sb-addr=$host_ip \
-                                 --db-sb-cluster-local-addr=$host_ip \
-                                 run_sb_ovsdb
-        ;;
-        "ovn-sb-cluster-join") /usr/share/ovn/scripts/ovn-ctl \
-                               --db-sb-addr=$host_ip \
-                               --db-sb-cluster-local-addr=$host_ip \
-                               --db-sb-cluster-remote-addr=$remote_host \
-                               run_sb_ovsdb
-        ;;
-        "ovn-controller") ovn-controller --pidfile \
-                          --log-file=/var/log/ovn/ovn-controller.log
-        ;;
-        *) echo "$0 [ovn-nb-tcp|ovn-sb-tcp|ovn-northd-tcp|ovn-controller
-                     |ovn-nb-cluster-create|ovn-nb-cluster-join
-                     |ovn-sb-cluster-create|ovn-sb-cluster-join
-                     |ovn-northd-cluster]"
-esac