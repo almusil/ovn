almusil/ovn

commit 53e0d5ac2a74855d34ff41f224fae43e78190b89

controller: Learn EVPN workload FDB from the ingress traffic.
Add a learn flow per EVPN binding, this flow will create flows for
corresponding workloads that are ingressing from EVPN tunnel. This
allows us to avoid flooding of traffic towards unknown workloads.
The dynamic FDBs are learned only for things that are not advertised
by the EVPN itself, in other words it doesn't affect or overwrite the
static FDBs.

Reported-at: https://issues.redhat.com/browse/FDP-1673
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/physical.c
@@ -2977,6 +2977,7 @@ physical_consider_evpn_binding(const struct evpn_binding *binding,
 
     put_load(binding->dp_key, MFF_LOG_DATAPATH, 0, 32, ofpacts);
     put_load(binding->binding_key, MFF_LOG_INPORT, 0, 32, ofpacts);
+    put_resubmit(OFTABLE_LEARN_REMOTE_FDB, ofpacts);
     put_resubmit(OFTABLE_LOG_INGRESS_PIPELINE, ofpacts);
 
     ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 1050,
@@ -3021,6 +3022,61 @@ physical_consider_evpn_binding(const struct evpn_binding *binding,
     ofpact_put_OUTPUT(ofpacts)->port = binding->tunnel_ofport;
 
     ofctrl_add_flow(flow_table, OFTABLE_REMOTE_VTEP_OUTPUT, 55,
+                    binding->flow_uuid.parts[0],
+                    match, ofpacts, &binding->flow_uuid);
+
+    /* Dynamic FDB learn flows. */
+    ofpbuf_clear(ofpacts);
+    match_init_catchall(match);
+
+    match_set_metadata(match, htonll(binding->dp_key));
+    match_set_reg(match, MFF_LOG_INPORT - MFF_REG0, binding->binding_key);
+
+    /* This has to match the flow format installed by
+     * physical_consider_evpn_fdb(). */
+    size_t ol_offset = ofpacts->size;
+    struct ofpact_learn_spec *ol_spec;
+    struct ofpact_learn *ol = ofpact_put_LEARN(ofpacts);
+    ol->cookie = htonll(binding->flow_uuid.parts[0]);
+    ol->flags = NX_LEARN_F_DELETE_LEARNED;
+    ol->priority = 150;
+    ol->table_id = OFTABLE_GET_REMOTE_FDB;
+    ol->idle_timeout = binding->fdb_age_threshold;
+
+    /* Match the learned flow on the same metadata. */
+    ol_spec = ofpbuf_put_zeros(ofpacts, sizeof *ol_spec);
+    ol_spec->dst.field = mf_from_id(MFF_METADATA);
+    ol_spec->dst.ofs = 0;
+    ol_spec->dst.n_bits = ol_spec->dst.field->n_bits;
+    ol_spec->n_bits = ol_spec->dst.n_bits;
+    ol_spec->dst_type = NX_LEARN_DST_MATCH;
+    ol_spec->src_type = NX_LEARN_SRC_FIELD;
+    ol_spec->src.field = mf_from_id(MFF_METADATA);
+
+    /* Match ETH_DST on ETH_SRC of the incoming packet. */
+    ol_spec = ofpbuf_put_zeros(ofpacts, sizeof *ol_spec);
+    ol_spec->dst.field = mf_from_id(MFF_ETH_DST);
+    ol_spec->dst.ofs = 0;
+    ol_spec->dst.n_bits = ol_spec->dst.field->n_bits;
+    ol_spec->n_bits = ol_spec->dst.n_bits;
+    ol_spec->dst_type = NX_LEARN_DST_MATCH;
+    ol_spec->src_type = NX_LEARN_SRC_FIELD;
+    ol_spec->src.field = mf_from_id(MFF_ETH_SRC);
+
+    /* Load the INPORT value into REMOTE_OUTPORT. */
+    ol_spec = ofpbuf_put_zeros(ofpacts, sizeof *ol_spec);
+    ol_spec->dst.field = mf_from_id(MFF_LOG_REMOTE_OUTPORT);
+    ol_spec->dst.ofs = 0;
+    ol_spec->dst.n_bits = ol_spec->dst.field->n_bits;
+    ol_spec->n_bits = ol_spec->dst.n_bits;
+    ol_spec->dst_type = NX_LEARN_DST_LOAD;
+    ol_spec->src_type = NX_LEARN_SRC_FIELD;
+    ol_spec->src.field = mf_from_id(MFF_LOG_INPORT);
+
+    ol = ofpbuf_at_assert(ofpacts, ol_offset, sizeof *ol);
+    ofpact_finish_LEARN(ofpacts, &ol);
+
+    ofctrl_add_flow(flow_table, OFTABLE_LEARN_REMOTE_FDB, 100,
                     binding->flow_uuid.parts[0],
                     match, ofpacts, &binding->flow_uuid);
 }
@@ -3078,6 +3134,7 @@ physical_consider_evpn_fdb(const struct evpn_fdb *fdb,
                            struct ofpbuf *ofpacts, struct match *match,
                            struct ovn_desired_flow_table *flow_table)
 {
+    /* Static FDB flow. */
     ofpbuf_clear(ofpacts);
     match_init_catchall(match);
 
@@ -3086,6 +3143,18 @@ physical_consider_evpn_fdb(const struct evpn_fdb *fdb,
 
     put_load(fdb->binding_key, MFF_LOG_REMOTE_OUTPORT, 0, 32, ofpacts);
     ofctrl_add_flow(flow_table, OFTABLE_GET_REMOTE_FDB, 150,
+                    fdb->flow_uuid.parts[0],
+                    match, ofpacts, &fdb->flow_uuid);
+
+    /* Prevent dynamic learning if it's already known via static FDB. */
+    ofpbuf_clear(ofpacts);
+    match_init_catchall(match);
+
+    match_set_metadata(match, htonll(fdb->dp_key));
+    match_set_reg(match, MFF_LOG_INPORT - MFF_REG0, fdb->binding_key);
+    match_set_dl_src(match, fdb->mac);
+
+    ofctrl_add_flow(flow_table, OFTABLE_LEARN_REMOTE_FDB, 150,
                     fdb->flow_uuid.parts[0],
                     match, ofpacts, &fdb->flow_uuid);
 }

tests/system-ovn.at
@@ -18438,7 +18438,7 @@ OVS_WAIT_FOR_OUTPUT_UNQUOTED([ovn-appctl evpn/vtep-multicast-group-list | cut -d
 
 AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_PHY_TO_LOG | grep priority=1050 | \
                    awk '{print $7, $8}' | sort], [0], [dnl
-priority=1050,tun_id=0xa,tun_src=169.0.0.10,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000001->NXM_NX_REG14[[]],resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
+priority=1050,tun_id=0xa,tun_src=169.0.0.10,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000001->NXM_NX_REG14[[]],resubmit(,OFTABLE_LEARN_REMOTE_FDB),resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
 ])
 
 AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_VTEP_OUTPUT | grep output | \
@@ -18448,6 +18448,11 @@ priority=50,reg15=0x80000001,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_
 priority=50,reg15=0x8001,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],load:0xa900000a->NXM_NX_TUN_IPV4_DST[[]],output:$ofport,resubmit(,OFTABLE_LOCAL_OUTPUT)
 priority=50,reg15=0x8004,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],load:0xa900000a->NXM_NX_TUN_IPV4_DST[[]],output:$ofport,resubmit(,OFTABLE_LOCAL_OUTPUT)
 priority=55,reg10=0x1/0x1,reg15=0x80000001,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa900000a->NXM_NX_TUN_IPV4_DST[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],load:0xffff->NXM_OF_IN_PORT[[]],output:$ofport
+])
+
+AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
+                   awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
 ])
 
 # Simulate second remote VTEP.
@@ -18471,8 +18476,8 @@ AT_CHECK([ovn-appctl evpn/vtep-multicast-group-list | wc -l], [0], [1
 
 AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_PHY_TO_LOG | grep priority=1050 | \
                    awk '{print $7, $8}' | sort], [0], [dnl
-priority=1050,tun_id=0xa,tun_src=169.0.0.10,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000001->NXM_NX_REG14[[]],resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
-priority=1050,tun_id=0xa,tun_src=169.0.0.20,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000002->NXM_NX_REG14[[]],resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
+priority=1050,tun_id=0xa,tun_src=169.0.0.10,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000001->NXM_NX_REG14[[]],resubmit(,OFTABLE_LEARN_REMOTE_FDB),resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
+priority=1050,tun_id=0xa,tun_src=169.0.0.20,tun_dst=169.0.0.1,in_port=$ofport actions=load:0x$dp_key->OXM_OF_METADATA[[0..31]],load:0x80000002->NXM_NX_REG14[[]],resubmit(,OFTABLE_LEARN_REMOTE_FDB),resubmit(,OFTABLE_LOG_INGRESS_PIPELINE)
 ])
 
 ovs-ofctl dump-flows br-int table=OFTABLE_REMOTE_VTEP_OUTPUT > oftable_remote_vtep_output
@@ -18482,6 +18487,12 @@ priority=50,reg15=0x80000001,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_
 priority=50,reg15=0x80000002,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa9000014->NXM_NX_TUN_IPV4_DST[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],output:$ofport
 priority=55,reg10=0x1/0x1,reg15=0x80000001,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa900000a->NXM_NX_TUN_IPV4_DST[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],load:0xffff->NXM_OF_IN_PORT[[]],output:$ofport
 priority=55,reg10=0x1/0x1,reg15=0x80000002,metadata=0x$dp_key actions=load:0xa9000001->NXM_NX_TUN_IPV4_SRC[[]],load:0xa9000014->NXM_NX_TUN_IPV4_DST[[]],load:0xa->NXM_NX_TUN_ID[[0..23]],load:0xffff->NXM_OF_IN_PORT[[]],output:$ofport
+])
+
+AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
+                   awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
 ])
 
 AT_CHECK([grep "resubmit" oftable_remote_vtep_output | grep -c "load:0xa900000a"], [0], [3
@@ -18503,6 +18514,14 @@ AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_GET_REMOTE_FDB | gr
 priority=0 actions=load:0->NXM_NX_REG1[[]]
 priority=150,metadata=0x$dp_key,dl_dst=f0:00:0f:16:10:50 actions=load:0x80000001->NXM_NX_REG1[[]]
 priority=150,metadata=0x$dp_key,dl_dst=f0:00:0f:16:10:60 actions=load:0x80000002->NXM_NX_REG1[[]]
+])
+
+AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
+                   awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=150,reg14=0x80000001,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:50 actions=drop
+priority=150,reg14=0x80000002,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:60 actions=drop
 ])
 
 # Check that the recompute won't change the UUIDs and tunnel keys.
@@ -18539,6 +18558,25 @@ f0:00:0f:16:01:10 dev lo-10 vlan 1 master br-10 static
 
 check ovn-nbctl --wait=hv remove logical_switch ls-evpn other_config dynamic-routing-redistribute
 OVS_WAIT_FOR_OUTPUT([bridge fdb show | grep "lo-10" | grep "f0:00:0f:16:01" | sort], [0], [dnl
+])
+
+check ovn-nbctl --wait=hv set logical_switch ls-evpn other_config:fdb_age_threshold=300
+ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB
+AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
+                   awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,idle_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,idle_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=150,reg14=0x80000001,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:50 actions=drop
+priority=150,reg14=0x80000002,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:60 actions=drop
+])
+
+check ovn-nbctl --wait=hv remove logical_switch ls-evpn other_config fdb_age_threshold
+AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
+                   awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=150,reg14=0x80000001,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:50 actions=drop
+priority=150,reg14=0x80000002,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:60 actions=drop
 ])
 
 AS_BOX([L2 EVPN ARP learning])

controller/evpn-binding.c
@@ -121,6 +121,14 @@ evpn_binding_run(const struct evpn_binding_ctx_in *b_ctx_in,
             updated = true;
         }
 
+        uint16_t fdb_age_threshold =
+            MIN(smap_get_uint(&edp->ldp->datapath->external_ids,
+                              "fdb_age_threshold", 0), UINT16_MAX);
+        if (binding->fdb_age_threshold != fdb_age_threshold) {
+            binding->fdb_age_threshold = fdb_age_threshold;
+            updated = true;
+        }
+
         if (updated) {
             hmapx_add(b_ctx_out->updated_bindings, binding);
 

controller/evpn-binding.h
@@ -64,6 +64,7 @@ struct evpn_binding {
 
     ofp_port_t tunnel_ofport;
     uint32_t dp_key;
+    uint16_t fdb_age_threshold;
 };
 
 struct evpn_datapath {

controller/lflow.h
@@ -103,6 +103,7 @@ struct uuid;
 #define OFTABLE_CT_STATE_SAVE            85
 #define OFTABLE_CT_ORIG_PROTO_LOAD       86
 #define OFTABLE_GET_REMOTE_FDB           87
+#define OFTABLE_LEARN_REMOTE_FDB         88
 
 /* Common defines shared between some controller components. */
 #define CHASSIS_FLOOD_INDEX_START 0x8000

tests/multinode.at
@@ -3693,6 +3693,7 @@ check m_as ovn-gw-1 ip netns exec fabric_workload ip link set evpn_host addr 00:
 check m_as ovn-gw-1 ip netns exec fabric_workload ip addr add dev evpn_host 10.0.0.41/24
 check m_as ovn-gw-1 ip netns exec fabric_workload ip link set evpn_host up
 check m_as ovn-gw-1 ip netns exec fabric_workload ip r a default via 10.0.0.1
+check m_as ovn-gw-1 ip netns exec frr-ns ip link set address 00:00:10:00:00:81 dev br-10
 check m_as ovn-gw-1 ip netns exec frr-ns ip a a dev br-10 10.0.0.81/24
 
 check m_as ovn-gw-2 ip netns add fabric_workload
@@ -3707,6 +3708,7 @@ check m_as ovn-gw-2 ip netns exec fabric_workload ip link set evpn_host addr 00:
 check m_as ovn-gw-2 ip netns exec fabric_workload ip addr add dev evpn_host 10.0.0.42/24
 check m_as ovn-gw-2 ip netns exec fabric_workload ip link set evpn_host up
 check m_as ovn-gw-2 ip netns exec fabric_workload ip r a default via 10.0.0.1
+check m_as ovn-gw-2 ip netns exec frr-ns ip link set address 00:00:10:00:00:82 dev br-10
 check m_as ovn-gw-2 ip netns exec frr-ns ip a a dev br-10 10.0.0.82/24
 
 AS_BOX([Checking EVPN MACs on External BGP host])
@@ -3809,6 +3811,18 @@ AS_BOX([Check traffic to "fabric" hosts - ping from internal hosts])
 OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec w-int1 ping -W 1 -c 1 10.0.0.41])
 OVS_WAIT_UNTIL([m_as ovn-gw-2 ip netns exec w-int2 ping -W 1 -c 1 10.0.0.42])
 
+AS_BOX([Check traffic to "fabric" hosts - ping from VNI bridge])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-10 ping -W 1 -c 3 10.0.0.11])
+OVS_WAIT_UNTIL([m_as ovn-gw-2 ip netns exec frr-ns ip vrf exec vrf-10 ping -W 1 -c 3 10.0.0.12])
+
+# Check if the dynamic FDB was created.
+OVS_WAIT_FOR_OUTPUT([m_as ovn-gw-1 ovs-ofctl dump-flows br-int table=OFTABLE_GET_REMOTE_FDB,dl_dst=00:00:10:00:00:81 | grep -c "cookie"], [0], [dnl
+1
+])
+OVS_WAIT_FOR_OUTPUT([m_as ovn-gw-2 ovs-ofctl dump-flows br-int table=OFTABLE_GET_REMOTE_FDB,dl_dst=00:00:10:00:00:82 | grep -c "cookie"], [0], [dnl
+1
+])
+
 # Remove "workloads" (VIF LSPs) on both chassis.
 check multinode_nbctl --wait=hv lsp-del w1 -- lsp-del w2
 
@@ -3830,6 +3844,11 @@ Flags: N=sync-neighs, I=local-inactive, P=peer-active, X=peer-proxy
 MAC               Type   Flags Intf/Remote ES/VTEP            VLAN  Seq #'s
 00:00:00:00:02:00 local        evpn_host_peer                       0/0
 ])
+
+# Check if the dynamic FDB was removed.
+check multinode_nbctl --wait=hv remove logical_switch ls other_config dynamic-routing-vni
+OVS_WAIT_FOR_OUTPUT([m_as ovn-gw-1 ovs-ofctl dump-flows br-int table=OFTABLE_GET_REMOTE_FDB,dl_dst=00:00:10:00:00:81 | grep -q "cookie"], [1])
+OVS_WAIT_FOR_OUTPUT([m_as ovn-gw-2 ovs-ofctl dump-flows br-int table=OFTABLE_GET_REMOTE_FDB,dl_dst=00:00:10:00:00:82 | grep -q "cookie"], [1])
 
 AT_CLEANUP
 

tests/ovn-macros.at
@@ -1517,5 +1517,6 @@ m4_define([OFTABLE_FLOOD_REMOTE_CHASSIS], [84])
 m4_define([OFTABLE_CT_STATE_SAVE], [85])
 m4_define([OFTABLE_CT_ORIG_PROTO_LOAD], [86])
 m4_define([OFTABLE_GET_REMOTE_FDB], [87])
+m4_define([OFTABLE_LEARN_REMOTE_FDB], [88])
 
 m4_define([OFTABLE_SAVE_INPORT_HEX], [m4_eval(OFTABLE_SAVE_INPORT, 16)])

commit 5ccc71c7d04dd15fa3d97a8c33783d06b4f5f019

controller: Switch idle_timeout to hard_timeout for dynamic EVPN FDBs.
The dynamically learned FBDs were using idle_timeout configured to
the same value as fdb_age_threshold for given LS. This could cause
an issue when the traffic was originated only from OVN, without the
reply the flow would be used, but OVN could be just blackholing
traffic.

Switch to hard_timeout instead, that means the flow will be removed
after the specified timeout regardless of the usage, however
this is still better than the blackhole that isn't recoverable.
Without the FDB we would flood instead, which might happen during the
period when hard_timeout expires before the workload replies back to
populate the FDB again.

Fixes: 53e0d5ac2a74 ("controller: Learn EVPN workload FDB from the ingress traffic.")
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/physical.c
@@ -2965,7 +2965,7 @@ physical_consider_evpn_binding(const struct evpn_binding *binding,
     ol->flags = NX_LEARN_F_DELETE_LEARNED;
     ol->priority = 150;
     ol->table_id = OFTABLE_GET_REMOTE_FDB;
-    ol->idle_timeout = binding->fdb_age_threshold;
+    ol->hard_timeout = binding->fdb_age_threshold;
 
     /* Match the learned flow on the same metadata. */
     ol_spec = ofpbuf_put_zeros(ofpacts, sizeof *ol_spec);

tests/system-ovn.at
@@ -18653,9 +18653,9 @@ check ovn-nbctl --wait=hv set logical_switch ls-evpn other_config:fdb_age_thresh
 ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB
 AT_CHECK_UNQUOTED([ovs-ofctl dump-flows br-int table=OFTABLE_LEARN_REMOTE_FDB | grep priority | \
                    awk '{print $7, $8}' | strip_cookie | sort], [0], [dnl
-priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,idle_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
-priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,idle_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
-priority=100,reg14=0x80000003,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,idle_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000001,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,hard_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000002,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,hard_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
+priority=100,reg14=0x80000003,metadata=0x$dp_key actions=learn(table=OFTABLE_GET_REMOTE_FDB,hard_timeout=300,priority=150,delete_learned,OXM_OF_METADATA[[]],NXM_OF_ETH_DST[[]]=NXM_OF_ETH_SRC[[]],load:NXM_NX_REG14[[]]->NXM_NX_REG1[[]])
 priority=150,reg14=0x80000001,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:50 actions=drop
 priority=150,reg14=0x80000002,metadata=0x$dp_key,dl_src=f0:00:0f:16:10:60 actions=drop
 priority=150,reg14=0x80000003,metadata=0x$dp_key,dl_src=00:00:00:00:00:01 actions=drop