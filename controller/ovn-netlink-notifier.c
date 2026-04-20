almusil/ovn

commit 5ec13ae6eff6922354a507a68bbfbeee5f784ee9

controller: Consolidate the netlink notifiers.
The netlink notifiers used a lot of code that was more or less
identical to each other. Extract the common code into separate module
which allows the definition of listeners and their specific data.
This should make it easier to add any new notifier, which will be the
case in the future. It should also make it slightly easier to track
individual updates and changes that could be processed incrementally
instead of full recompute when there is any change.

Acked-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/ovn-netlink-notifier.c
@@ -1,0 +1,251 @@
+/* Copyright (c) 2026, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <linux/rtnetlink.h>
+#include <net/if.h>
+
+#include "neighbor-exchange-netlink.h"
+#include "netlink-notifier.h"
+#include "route-exchange-netlink.h"
+#include "route-table.h"
+#include "vec.h"
+
+#include "openvswitch/vlog.h"
+
+#include "ovn-netlink-notifier.h"
+
+VLOG_DEFINE_THIS_MODULE(ovn_netlink_notifier);
+
+#define NOTIFIER_MSGS_CAPACITY_THRESHOLD 1024
+
+struct ovn_netlink_notifier {
+    /* Group for which we want to receive the notification. */
+    int group;
+    /* The notifier pointers. */
+    struct nln_notifier *nln_notifier;
+    /* Messages received by given notifier. */
+    struct vector msgs;
+    /* Notifier change handler. */
+    nln_notify_func *change_handler;
+    /* Name of the notifier. */
+    const char *name;
+};
+
+union ovn_notifier_msg_change {
+    struct route_table_msg route;
+    struct ne_table_msg neighbor;
+};
+
+static void ovn_netlink_route_change_handler(const void *change_, void *aux);
+static void ovn_netlink_neighbor_change_handler(const void *change_,
+                                                void *aux);
+
+static struct ovn_netlink_notifier notifiers[OVN_NL_NOTIFIER_MAX] = {
+    [OVN_NL_NOTIFIER_ROUTE_V4] = {
+        .group = RTNLGRP_IPV4_ROUTE,
+        .msgs = VECTOR_EMPTY_INITIALIZER(uint32_t),
+        .change_handler = ovn_netlink_route_change_handler,
+        .name = "route-ipv4",
+    },
+    [OVN_NL_NOTIFIER_ROUTE_V6] = {
+        .group = RTNLGRP_IPV6_ROUTE,
+        .msgs = VECTOR_EMPTY_INITIALIZER(uint32_t),
+        .change_handler = ovn_netlink_route_change_handler,
+        .name = "route-ipv6",
+    },
+    [OVN_NL_NOTIFIER_NEIGHBOR] = {
+        .group = RTNLGRP_NEIGH,
+        .msgs = VECTOR_EMPTY_INITIALIZER(struct ne_table_msg),
+        .change_handler = ovn_netlink_neighbor_change_handler,
+        .name = "neighbor",
+    },
+};
+
+static struct nln *nln_handle;
+static union ovn_notifier_msg_change nln_msg_change;
+
+static int
+ovn_netlink_notifier_parse(struct ofpbuf *buf, void *change_)
+{
+    struct nlmsghdr *nlmsg = ofpbuf_at(buf, 0, NLMSG_HDRLEN);
+    if (!nlmsg) {
+        return 0;
+    }
+
+    union ovn_notifier_msg_change *change = change_;
+    if (nlmsg->nlmsg_type == RTM_NEWROUTE ||
+        nlmsg->nlmsg_type == RTM_DELROUTE) {
+        return route_table_parse(buf, &change->route);
+    }
+
+    if (nlmsg->nlmsg_type == RTM_NEWNEIGH ||
+        nlmsg->nlmsg_type == RTM_DELNEIGH) {
+        return ne_table_parse(buf, &change->neighbor);
+    }
+
+    return 0;
+}
+
+static void
+ovn_netlink_route_change_handler(const void *change_, void *aux)
+{
+    if (!change_) {
+        return;
+    }
+
+    struct ovn_netlink_notifier *notifier = aux;
+    union ovn_notifier_msg_change *change =
+        CONST_CAST(union ovn_notifier_msg_change *, change_);
+
+    struct route_data *rd = &change->route.rd;
+    if (rd->rtm_protocol != RTPROT_OVN) {
+        /* We just cannot copy the whole route_data because it has reference
+         * to self for the nexthop list. */
+        vector_push(&notifier->msgs, &rd->rta_table_id);
+    }
+
+    route_data_destroy(rd);
+}
+
+static void
+ovn_netlink_neighbor_change_handler(const void *change_, void *aux)
+{
+    if (!change_) {
+        return;
+    }
+
+    struct ovn_netlink_notifier *notifier = aux;
+    const union ovn_notifier_msg_change *change = change_;
+
+    if (!ne_is_ovn_owned(&change->neighbor.nd)) {
+        vector_push(&notifier->msgs, &change->neighbor);
+    }
+}
+
+static void
+ovn_netlink_register_notifier(enum ovn_netlink_notifier_type type)
+{
+    ovs_assert(type < OVN_NL_NOTIFIER_MAX);
+
+    struct ovn_netlink_notifier *notifier = &notifiers[type];
+    if (notifier->nln_notifier) {
+        return;
+    }
+
+    VLOG_INFO("Adding %s table watchers.", notifier->name);
+    if (!nln_handle) {
+        nln_handle = nln_create(NETLINK_ROUTE, ovn_netlink_notifier_parse,
+                                &nln_msg_change);
+        ovs_assert(nln_handle);
+    }
+
+    notifier->nln_notifier = nln_notifier_create(nln_handle, notifier->group,
+                                                 notifier->change_handler,
+                                                 notifier);
+
+    if (!notifier->nln_notifier) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "Failed to create %s table watcher.",
+                     notifier->name);
+    }
+}
+
+static void
+ovn_netlink_deregister_notifier(enum ovn_netlink_notifier_type type)
+{
+    ovs_assert(type < OVN_NL_NOTIFIER_MAX);
+
+    struct ovn_netlink_notifier *notifier = &notifiers[type];
+    if (!notifier->nln_notifier) {
+        return;
+    }
+
+    VLOG_INFO("Removing %s table watchers.", notifier->name);
+    nln_notifier_destroy(notifier->nln_notifier);
+    notifier->nln_notifier = NULL;
+
+    size_t i;
+    for (i = 0; i < OVN_NL_NOTIFIER_MAX; i++) {
+        if (notifiers[i].nln_notifier) {
+            break;
+        }
+    }
+
+    if (i == OVN_NL_NOTIFIER_MAX) {
+        /* This was the last notifier, destroy the handle too. */
+        nln_destroy(nln_handle);
+        nln_handle = NULL;
+    }
+}
+
+void
+ovn_netlink_update_notifier(enum ovn_netlink_notifier_type type, bool enabled)
+{
+    if (enabled) {
+        ovn_netlink_register_notifier(type);
+    } else {
+        ovn_netlink_deregister_notifier(type);
+    }
+}
+
+struct vector *
+ovn_netlink_get_msgs(enum ovn_netlink_notifier_type type)
+{
+    ovs_assert(type < OVN_NL_NOTIFIER_MAX);
+    return &notifiers[type].msgs;
+}
+
+void
+ovn_netlink_notifier_flush(enum ovn_netlink_notifier_type type)
+{
+    ovs_assert(type < OVN_NL_NOTIFIER_MAX);
+    struct ovn_netlink_notifier *notifier = &notifiers[type];
+    vector_clear(&notifier->msgs);
+}
+
+void
+ovn_netlink_notifiers_run(void)
+{
+    for (size_t i = 0; i < OVN_NL_NOTIFIER_MAX; i++) {
+        if (vector_capacity(&notifiers[i].msgs) >
+            NOTIFIER_MSGS_CAPACITY_THRESHOLD) {
+            vector_shrink_to_fit(&notifiers[i].msgs);
+        }
+    }
+
+    if (nln_handle) {
+        nln_run(nln_handle);
+    }
+}
+
+void
+ovn_netlink_notifiers_wait(void)
+{
+    if (nln_handle) {
+        nln_wait(nln_handle);
+    }
+}
+
+void
+ovn_netlink_notifiers_destroy(void)
+{
+    for (size_t i = 0; i < OVN_NL_NOTIFIER_MAX; i++) {
+        ovn_netlink_notifier_flush(i);
+        ovn_netlink_deregister_notifier(i);
+        vector_destroy(&notifiers[i].msgs);
+    }
+}

controller/automake.mk
@@ -32,6 +32,7 @@ controller_ovn_controller_SOURCES = \
 	controller/lport.h \
 	controller/ofctrl.c \
 	controller/ofctrl.h \
+	controller/ovn-netlink-notifier.h \
 	controller/neighbor.c \
 	controller/neighbor.h \
 	controller/neighbor-of.c \
@@ -63,33 +64,29 @@ controller_ovn_controller_SOURCES = \
 	controller/ecmp-next-hop-monitor.h \
 	controller/ecmp-next-hop-monitor.c \
 	controller/route-exchange.h \
-	controller/route-table-notify.h \
 	controller/route.h \
 	controller/route.c \
 	controller/garp_rarp.h \
 	controller/garp_rarp.c \
 	controller/neighbor-exchange.h \
-	controller/neighbor-table-notify.h \
 	controller/host-if-monitor.h
 
 if HAVE_NETLINK
 controller_ovn_controller_SOURCES += \
 	controller/host-if-monitor.c \
+	controller/ovn-netlink-notifier.c \
 	controller/neighbor-exchange-netlink.h \
 	controller/neighbor-exchange-netlink.c \
 	controller/neighbor-exchange.c \
-	controller/neighbor-table-notify.c \
 	controller/route-exchange-netlink.h \
 	controller/route-exchange-netlink.c \
-	controller/route-exchange.c \
-	controller/route-table-notify.c
+	controller/route-exchange.c
 else
 controller_ovn_controller_SOURCES += \
 	controller/host-if-monitor-stub.c \
+	controller/ovn-netlink-notifier-stub.c \
 	controller/neighbor-exchange-stub.c \
-	controller/neighbor-table-notify-stub.c \
-	controller/route-exchange-stub.c \
-	controller/route-table-notify-stub.c
+	controller/route-exchange-stub.c
 endif
 
 controller_ovn_controller_LDADD = lib/libovn.la $(OVS_LIBDIR)/libopenvswitch.la

controller/neighbor-exchange.c
@@ -21,7 +21,6 @@
 #include "neighbor.h"
 #include "neighbor-exchange.h"
 #include "neighbor-exchange-netlink.h"
-#include "neighbor-table-notify.h"
 #include "openvswitch/poll-loop.h"
 #include "openvswitch/vlog.h"
 #include "ovn-util.h"
@@ -136,8 +135,7 @@ neighbor_exchange_run(const struct neighbor_exchange_ctx_in *n_ctx_in,
             break;
         }
 
-        neighbor_table_add_watch_request(&n_ctx_out->neighbor_table_watches,
-                                         if_index, nim->if_name);
+        vector_push(n_ctx_out->neighbor_table_watches, &if_index);
         vector_destroy(&received_neighbors);
     }
 }

controller/neighbor-exchange.h
@@ -30,8 +30,8 @@ struct neighbor_exchange_ctx_in {
 };
 
 struct neighbor_exchange_ctx_out {
-    /* Contains struct neighbor_table_watch_request. */
-    struct hmap neighbor_table_watches;
+    /* Contains int32_t representing if_index. */
+    struct vector *neighbor_table_watches;
     /* Contains 'struct evpn_remote_vtep'. */
     struct hmap *remote_vteps;
     /* Contains 'struct evpn_static_entry', remote FDB entries learned through

controller/neighbor-table-notify-stub.c
@@ -1,57 +1,0 @@
-/* Copyright (c) 2025, Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include <config.h>
-
-#include <stdbool.h>
-
-#include "openvswitch/compiler.h"
-#include "neighbor-table-notify.h"
-
-bool
-neighbor_table_notify_run(void)
-{
-    return false;
-}
-
-void
-neighbor_table_notify_wait(void)
-{
-}
-
-void
-neighbor_table_add_watch_request(
-    struct hmap *neighbor_table_watches OVS_UNUSED,
-    int32_t if_index OVS_UNUSED,
-    const char *if_name OVS_UNUSED)
-{
-}
-
-void
-neighbor_table_watch_request_cleanup(
-    struct hmap *neighbor_table_watches OVS_UNUSED)
-{
-}
-
-void
-neighbor_table_notify_update_watches(
-    const struct hmap *neighbor_table_watches OVS_UNUSED)
-{
-}
-
-void
-neighbor_table_notify_destroy(void)
-{
-}

controller/neighbor-table-notify.c
@@ -1,244 +1,0 @@
-/* Copyright (c) 2025, Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include <config.h>
-
-#include <linux/rtnetlink.h>
-#include <net/if.h>
-
-#include "hash.h"
-#include "hmapx.h"
-#include "lib/util.h"
-#include "netlink-notifier.h"
-#include "openvswitch/vlog.h"
-
-#include "neighbor-exchange-netlink.h"
-#include "neighbor-table-notify.h"
-
-VLOG_DEFINE_THIS_MODULE(neighbor_table_notify);
-
-struct neighbor_table_watch_request {
-    struct hmap_node node;
-    int32_t if_index;
-    char if_name[IFNAMSIZ + 1];
-};
-
-struct neighbor_table_watch_entry {
-    struct hmap_node node;
-    int32_t if_index;
-    char if_name[IFNAMSIZ + 1];
-};
-
-static struct hmap watches = HMAP_INITIALIZER(&watches);
-static bool any_neighbor_table_changed;
-static struct ne_table_msg nln_nmsg_change;
-
-static struct nln *nl_neighbor_handle;
-static struct nln_notifier *nl_neighbor_notifier;
-
-static void neighbor_table_change(const void *change_, void *aux);
-
-static void
-neighbor_table_register_notifiers(void)
-{
-    VLOG_INFO("Adding neighbor table watchers.");
-    ovs_assert(!nl_neighbor_handle);
-
-    nl_neighbor_handle = nln_create(NETLINK_ROUTE, ne_table_parse,
-                                    &nln_nmsg_change);
-    ovs_assert(nl_neighbor_handle);
-
-    nl_neighbor_notifier =
-        nln_notifier_create(nl_neighbor_handle, RTNLGRP_NEIGH,
-                            neighbor_table_change, NULL);
-    if (!nl_neighbor_notifier) {
-        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
-        VLOG_WARN_RL(&rl, "Failed to create neighbor table watcher.");
-    }
-}
-
-static void
-neighbor_table_deregister_notifiers(void)
-{
-    VLOG_INFO("Removing neighbor table watchers.");
-    ovs_assert(nl_neighbor_handle);
-
-    nln_notifier_destroy(nl_neighbor_notifier);
-    nln_destroy(nl_neighbor_handle);
-    nl_neighbor_notifier = NULL;
-    nl_neighbor_handle = NULL;
-}
-
-static uint32_t
-neighbor_table_notify_hash_watch(int32_t if_index)
-{
-    /* To allow lookups triggered by netlink messages, don't include the
-     * if_name in the hash.  The netlink updates only include if_index. */
-    return hash_int(if_index, 0);
-}
-
-static void
-add_watch_entry(int32_t if_index, const char *if_name)
-{
-   VLOG_DBG("Registering new neighbor table watcher "
-            "for interface %s (%"PRId32").",
-            if_name, if_index);
-
-    struct neighbor_table_watch_entry *we;
-    uint32_t hash = neighbor_table_notify_hash_watch(if_index);
-    we = xzalloc(sizeof *we);
-    we->if_index = if_index;
-    ovs_strzcpy(we->if_name, if_name, sizeof we->if_name);
-    hmap_insert(&watches, &we->node, hash);
-
-    if (!nl_neighbor_handle) {
-        neighbor_table_register_notifiers();
-    }
-}
-
-static void
-remove_watch_entry(struct neighbor_table_watch_entry *we)
-{
-    VLOG_DBG("Removing neighbor table watcher for interface %s (%"PRId32").",
-             we->if_name, we->if_index);
-    hmap_remove(&watches, &we->node);
-    free(we);
-
-    if (hmap_is_empty(&watches)) {
-        neighbor_table_deregister_notifiers();
-    }
-}
-
-bool
-neighbor_table_notify_run(void)
-{
-    any_neighbor_table_changed = false;
-
-    if (nl_neighbor_handle) {
-        nln_run(nl_neighbor_handle);
-    }
-
-    return any_neighbor_table_changed;
-}
-
-void
-neighbor_table_notify_wait(void)
-{
-    if (nl_neighbor_handle) {
-        nln_wait(nl_neighbor_handle);
-    }
-}
-
-void
-neighbor_table_add_watch_request(struct hmap *neighbor_table_watches,
-                                 int32_t if_index, const char *if_name)
-{
-    struct neighbor_table_watch_request *wr = xzalloc(sizeof *wr);
-
-    wr->if_index = if_index;
-    ovs_strzcpy(wr->if_name, if_name, sizeof wr->if_name);
-    hmap_insert(neighbor_table_watches, &wr->node,
-                neighbor_table_notify_hash_watch(wr->if_index));
-}
-
-void
-neighbor_table_watch_request_cleanup(struct hmap *neighbor_table_watches)
-{
-    struct neighbor_table_watch_request *wr;
-    HMAP_FOR_EACH_POP (wr, node, neighbor_table_watches) {
-        free(wr);
-    }
-}
-
-static struct neighbor_table_watch_entry *
-find_watch_entry(int32_t if_index, const char *if_name)
-{
-    struct neighbor_table_watch_entry *we;
-    uint32_t hash = neighbor_table_notify_hash_watch(if_index);
-    HMAP_FOR_EACH_WITH_HASH (we, node, hash, &watches) {
-        if (if_index == we->if_index && !strcmp(if_name, we->if_name)) {
-            return we;
-        }
-    }
-    return NULL;
-}
-
-static struct neighbor_table_watch_entry *
-find_watch_entry_by_if_index(int32_t if_index)
-{
-    struct neighbor_table_watch_entry *we;
-    uint32_t hash = neighbor_table_notify_hash_watch(if_index);
-    HMAP_FOR_EACH_WITH_HASH (we, node, hash, &watches) {
-        if (if_index == we->if_index) {
-            return we;
-        }
-    }
-    return NULL;
-}
-
-void
-neighbor_table_notify_update_watches(const struct hmap *neighbor_table_watches)
-{
-    struct hmapx sync_watches = HMAPX_INITIALIZER(&sync_watches);
-    struct neighbor_table_watch_entry *we;
-    HMAP_FOR_EACH (we, node, &watches) {
-        hmapx_add(&sync_watches, we);
-    }
-
-    struct neighbor_table_watch_request *wr;
-    HMAP_FOR_EACH (wr, node, neighbor_table_watches) {
-        we = find_watch_entry(wr->if_index, wr->if_name);
-        if (we) {
-            hmapx_find_and_delete(&sync_watches, we);
-        } else {
-            add_watch_entry(wr->if_index, wr->if_name);
-        }
-    }
-
-    struct hmapx_node *node;
-    HMAPX_FOR_EACH (node, &sync_watches) {
-        remove_watch_entry(node->data);
-    }
-
-    hmapx_destroy(&sync_watches);
-}
-
-void
-neighbor_table_notify_destroy(void)
-{
-    struct neighbor_table_watch_entry *we;
-    HMAP_FOR_EACH_SAFE (we, node, &watches) {
-        remove_watch_entry(we);
-    }
-}
-
-static void
-neighbor_table_change(const void *change_, void *aux OVS_UNUSED)
-{
-    /* We currently track whether at least one recent neighbor table change
-     * was detected.  If that's the case already there's no need to
-     * continue. */
-    if (any_neighbor_table_changed) {
-        return;
-    }
-
-    const struct ne_table_msg *change = change_;
-
-    if (change && !ne_is_ovn_owned(&change->nd)) {
-        if (find_watch_entry_by_if_index(change->nd.if_index)) {
-            any_neighbor_table_changed = true;
-        }
-    }
-}

controller/neighbor-table-notify.h
@@ -1,45 +1,0 @@
-/* Copyright (c) 2025, Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef NEIGHBOR_TABLE_NOTIFY_H
-#define NEIGHBOR_TABLE_NOTIFY_H 1
-
-#include <stdbool.h>
-#include "openvswitch/hmap.h"
-
-/* Returns true if any neighbor table has changed enough that we need
- * to learn new neighbor entries. */
-bool neighbor_table_notify_run(void);
-void neighbor_table_notify_wait(void);
-
-/* Add a watch request to the hmap. The hmap should later be passed to
- * neighbor_table_notify_update_watches*/
-void neighbor_table_add_watch_request(struct hmap *neighbor_table_watches,
-                                      int32_t if_index, const char *if_name);
-
-/* Cleanup all watch request in the provided hmap that where added using
- * neighbor_table_add_watch_request. */
-void neighbor_table_watch_request_cleanup(
-    struct hmap *neighbor_table_watches);
-
-/* Updates the list of neighbor table watches that are currently active.
- * hmap should contain struct neighbor_table_watch_request */
-void neighbor_table_notify_update_watches(
-    const struct hmap *neighbor_table_watches);
-
-/* Cleans up all neighbor table watches. */
-void neighbor_table_notify_destroy(void);
-
-#endif /* NEIGHBOR_TABLE_NOTIFY_H */

commit 562babc9bb3bfc2c837447eb65fdd8674780c9dd

controller: Flush the netlink notifier message when we disable it.
The netlink notifier could retain messages even after being disabled
which could cause problems when it would be re-enabled. Make sure
we flush them.

Fixes: 5ec13ae6eff6 ("controller: Consolidate the netlink notifiers.")
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/ovn-netlink-notifier.c
@@ -217,6 +217,8 @@ ovn_netlink_deregister_notifier(enum ovn_netlink_notifier_type type)
         nln_destroy(nln_handle);
         nln_handle = NULL;
     }
+
+    ovn_netlink_notifier_flush(type);
 }
 
 void
@@ -287,7 +289,6 @@ void
 ovn_netlink_notifiers_destroy(void)
 {
     for (size_t i = 0; i < OVN_NL_NOTIFIER_MAX; i++) {
-        ovn_netlink_notifier_flush(i);
         ovn_netlink_deregister_notifier(i);
         vector_destroy(&notifiers[i].msgs);
     }