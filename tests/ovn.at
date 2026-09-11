almusil/ovn

commit 9fc291c889bb99292d53f8ff884f41022bfe99f2

controller: Update OFTABLE_MAC_CACHE_USE for ARP reply generated by the tracked device.
In order to make mac_binding ageing process more reliable, track ARP
replies/NA for the selected mac_binding raw in the OFTABLE_MAC_CACHE_USE
table.

Acked-by: Ales Musil <amusil@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

controller/lflow.c
@@ -1356,6 +1356,12 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
     struct match get_arp_match = MATCH_CATCHALL_INITIALIZER;
     struct match lookup_arp_match = MATCH_CATCHALL_INITIALIZER;
     struct match mb_cache_use_match = MATCH_CATCHALL_INITIALIZER;
+    struct match lookup_arp_for_stats_match = MATCH_CATCHALL_INITIALIZER;
+
+    match_set_dl_src(&lookup_arp_match, mac_addr);
+    match_set_metadata(&lookup_arp_match, htonll(pb->datapath->tunnel_key));
+    match_set_reg(&lookup_arp_match, MFF_LOG_INPORT - MFF_REG0,
+                  pb->tunnel_key);
 
     if (strchr(ip, '.')) {
         ovs_be32 ip_addr;
@@ -1367,11 +1373,17 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
 
         match_set_reg(&get_arp_match, 0, ntohl(ip_addr));
 
-        match_set_reg(&lookup_arp_match, 0, ntohl(ip_addr));
         match_set_dl_type(&lookup_arp_match, htons(ETH_TYPE_ARP));
+        lookup_arp_for_stats_match = lookup_arp_match;
+
+        match_set_reg(&lookup_arp_match, 0, ntohl(ip_addr));
 
         match_set_dl_type(&mb_cache_use_match, htons(ETH_TYPE_IP));
         match_set_nw_src(&mb_cache_use_match, ip_addr);
+
+        match_set_arp_opcode_masked(&lookup_arp_for_stats_match, 2, 0xff);
+        match_set_arp_spa_masked(&lookup_arp_for_stats_match, ip_addr,
+                                 htonl(0xffffffff));
     } else {
         struct in6_addr ip6;
         if (!ipv6_parse(ip, &ip6)) {
@@ -1383,21 +1395,22 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
         memcpy(&value, &ip6, sizeof(value));
         match_set_xxreg(&get_arp_match, 0, ntoh128(value));
 
-        match_set_xxreg(&lookup_arp_match, 0, ntoh128(value));
         match_set_dl_type(&lookup_arp_match, htons(ETH_TYPE_IPV6));
         match_set_nw_proto(&lookup_arp_match, 58);
         match_set_icmp_code(&lookup_arp_match, 0);
+        lookup_arp_for_stats_match = lookup_arp_match;
+
+        match_set_xxreg(&lookup_arp_match, 0, ntoh128(value));
 
         match_set_dl_type(&mb_cache_use_match, htons(ETH_TYPE_IPV6));
         match_set_ipv6_src(&mb_cache_use_match, &ip6);
+
+        match_set_icmp_type(&lookup_arp_for_stats_match, 136);
+        match_set_nd_target(&lookup_arp_for_stats_match, &ip6);
     }
 
     match_set_metadata(&get_arp_match, htonll(pb->datapath->tunnel_key));
     match_set_reg(&get_arp_match, MFF_LOG_OUTPORT - MFF_REG0, pb->tunnel_key);
-
-    match_set_metadata(&lookup_arp_match, htonll(pb->datapath->tunnel_key));
-    match_set_reg(&lookup_arp_match, MFF_LOG_INPORT - MFF_REG0,
-                  pb->tunnel_key);
 
     match_set_dl_src(&mb_cache_use_match, mac_addr);
     match_set_reg(&mb_cache_use_match, MFF_LOG_INPORT - MFF_REG0,
@@ -1418,7 +1431,6 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
     ofpbuf_clear(&ofpacts);
     put_load(&value, sizeof value, MFF_LOG_FLAGS, MLF_LOOKUP_MAC_BIT, 1,
              &ofpacts);
-    match_set_dl_src(&lookup_arp_match, mac_addr);
     ofctrl_add_flow(flow_table, OFTABLE_MAC_LOOKUP, priority,
                     b ? b->header_.uuid.parts[0] : smb->header_.uuid.parts[0],
                     &lookup_arp_match, &ofpacts,
@@ -1426,6 +1438,9 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
 
     if (b) {
         ofpbuf_clear(&ofpacts);
+        ofctrl_add_flow(flow_table, OFTABLE_MAC_CACHE_USE, priority,
+                        b->header_.uuid.parts[0], &lookup_arp_for_stats_match,
+                        &ofpacts, &b->header_.uuid);
         ofctrl_add_flow(flow_table, OFTABLE_MAC_CACHE_USE, priority,
                         b->header_.uuid.parts[0], &mb_cache_use_match,
                         &ofpacts, &b->header_.uuid);

tests/ovn.at
@@ -35733,7 +35733,11 @@ port_key_1=$(printf "0x%x" $(as hv1 fetch_column port_binding tunnel_key logical
 dp_key_2=$(printf "0x%x" $(as hv1 fetch_column datapath tunnel_key external_ids:name=gw-2))
 port_key_2=$(printf "0x%x" $(as hv1 fetch_column port_binding tunnel_key logical_port=gw-2-public))
 
-table=" table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
+table=" table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop"
@@ -35781,6 +35785,8 @@ OVS_WAIT_UNTIL([
 ])
 
 AT_CHECK_UNQUOTED([as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE --no-stats | strip_cookie | sort], [0], [dnl
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop
 ])

controller/mac-cache.c
@@ -361,7 +361,8 @@ mac_binding_stats_process_flow_stats(struct ovs_list *stats_list,
         .mac = ofp_stats->match.flow.dl_src
     };
 
-    if (ofp_stats->match.flow.dl_type == htons(ETH_TYPE_IP)) {
+    if (ofp_stats->match.flow.dl_type == htons(ETH_TYPE_IP) ||
+        ofp_stats->match.flow.dl_type == htons(ETH_TYPE_ARP)) {
         stats->data.mb.ip = in6_addr_mapped_ipv4(ofp_stats->match.flow.nw_src);
     } else {
         stats->data.mb.ip = ofp_stats->match.flow.ipv6_src;

commit b5aa44456cd89fd4d59bfbe83e3f193f6765f03f

lflow: Lift the reply limitation for ARP mac_cache_use flows.
The mac_cache_use would have flows for ARP replies to bump the
timestamp when we receive a reply to our probe. However, simple
ARP request from the MAC binding IP+MAC should be enough to indicate
that the entry is alive. Remove the response match and use plain
ARP with proper source MAC address and SPA.

Fixes: 9fc291c889bb ("controller: Update OFTABLE_MAC_CACHE_USE for ARP reply generated by the tracked device.")
Fixes: c23059318cb7 ("northd, controller: Use the MAC cache table")
Acked-by: Jacob Tanenbaum <jtanenba@redhat.com <echaudro@redhat.com>>
Signed-off-by: Ales Musil <amusil@redhat.com>
(cherry picked from commit d3c289579624c6edd73ad125d9d6486cd731fb4d)

controller/lflow.c
@@ -1369,7 +1369,6 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
         match_set_dl_type(&mb_cache_use_match, htons(ETH_TYPE_IP));
         match_set_nw_src(&mb_cache_use_match, ip_addr);
 
-        match_set_arp_opcode_masked(&lookup_arp_for_stats_match, 2, 0xff);
         match_set_arp_spa_masked(&lookup_arp_for_stats_match, ip_addr,
                                  htonl(0xffffffff));
     } else {

tests/ovn.at
@@ -36446,10 +36446,10 @@ port_key_1=$(printf "0x%x" $(as hv1 fetch_column port_binding tunnel_key logical
 dp_key_2=$(printf "0x%x" $(as hv1 fetch_column datapath tunnel_key external_ids:name=gw-2))
 port_key_2=$(printf "0x%x" $(as hv1 fetch_column port_binding tunnel_key logical_port=gw-2-public))
 
-table=" table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
- table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
- table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
- table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
+table=" table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_1},metadata=${dp_key_1},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
@@ -36498,8 +36498,8 @@ OVS_WAIT_UNTIL([
 ])
 
 AT_CHECK_UNQUOTED([as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE --no-stats | strip_cookie | sort], [0], [dnl
- table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10,arp_op=2 actions=drop
- table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20,arp_op=2 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,arp_spa=192.168.10.10 actions=drop
+ table=OFTABLE_MAC_CACHE_USE, priority=100,arp,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,arp_spa=192.168.10.20 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:10,nw_src=192.168.10.10 actions=drop
  table=OFTABLE_MAC_CACHE_USE, priority=100,ip,reg14=${port_key_2},metadata=${dp_key_2},dl_src=00:00:00:00:10:20,nw_src=192.168.10.20 actions=drop
 ])
@@ -36722,22 +36722,29 @@ ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
 check ovn-nbctl                                                     \
     -- ls-add ls1                                                   \
     -- ls-add ls2                                                   \
+    -- ls-add ls3                                                   \
     -- lr-add lr                                                    \
     -- set logical_router lr options:mac_binding_age_threshold=4    \
     -- lrp-add lr lr-ls1 00:00:00:00:10:00 192.168.10.1/24          \
     -- lrp-add lr lr-ls2 00:00:00:00:20:00 192.168.20.1/24          \
+    -- lrp-add lr lr-ls3 00:00:00:00:30:00 192.168.30.1/24          \
     -- lsp-add-router-port ls1 ls1-lr lr-ls1                        \
     -- lsp-add ls1 vif1                                             \
     -- lsp-set-addresses vif1 "00:00:00:00:10:10 192.168.10.10"     \
     -- lsp-add-router-port ls2 ls2-lr lr-ls2                        \
     -- lsp-add ls2 vif2                                             \
-    -- lsp-set-addresses vif2 "00:00:00:00:20:10 192.168.20.10"
+    -- lsp-set-addresses vif2 "00:00:00:00:20:10 192.168.20.10"     \
+    -- lsp-add-router-port ls3 ls3-lr lr-ls3                        \
+    -- lsp-add ls3 vif3                                             \
+    -- lsp-set-addresses vif3 "00:00:00:00:30:10 192.168.30.10"
 
 check ovs-vsctl                                      \
     -- add-port br-int vif1                          \
     -- set interface vif1 external-ids:iface-id=vif1 \
     -- add-port br-int vif2                          \
-    -- set interface vif2 external-ids:iface-id=vif2
+    -- set interface vif2 external-ids:iface-id=vif2 \
+    -- add-port br-int vif3                          \
+    -- set interface vif3 external-ids:iface-id=vif3
 
 OVN_POPULATE_ARP
 wait_for_ports_up
@@ -36754,23 +36761,32 @@ sleep 2
 send_garp hv1 vif1 2 00:00:00:00:10:10 ff:ff:ff:ff:ff:ff 192.168.10.10 192.168.10.10
 wait_row_count mac_binding 1 ip="192.168.10.10" logical_port="lr-ls1"
 
-uuid=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
+send_garp hv1 vif3 2 00:00:00:00:30:10 ff:ff:ff:ff:ff:ff 192.168.30.10 192.168.30.10
+wait_row_count mac_binding 1 ip="192.168.30.10" logical_port="lr-ls3"
+
+uuid_ls1=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
+uuid_ls3=$(fetch_column mac_binding _uuid ip="192.168.30.10" logical_port="lr-ls3")
 for i in $(seq 12); do
-    # Keep one entry alive by sending traffic that uses it.
+    # Keep two entries alive by sending traffic that uses them.
     send_udp hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:10 192.168.20.10 192.168.10.10
+    send_garp hv1 vif3 1 00:00:00:00:30:10 ff:ff:ff:ff:ff:ff 192.168.30.10 192.168.20.100
     sleep 1
     # The entry must not expire.
     check_row_count mac_binding 1 ip="192.168.10.10" logical_port="lr-ls1"
+    check_row_count mac_binding 1 ip="192.168.30.10" logical_port="lr-ls3"
     as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE
 done
 # Check that it's the same entry.
-uuid2=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
-check test "$uuid" = "$uuid2"
+uuid2_ls1=$(fetch_column mac_binding _uuid ip="192.168.10.10" logical_port="lr-ls1")
+uuid2_ls3=$(fetch_column mac_binding _uuid ip="192.168.30.10" logical_port="lr-ls3")
+check test "$uuid_ls1" = "$uuid2_ls1"
+check test "$uuid_ls3" = "$uuid2_ls3"
 
 # The other entry must have expired by now.
 check_row_count mac_binding 0 ip="192.168.20.10" logical_port="lr-ls2"
-# Wait for the alive one to expire as well.
+# Wait for the alive ones to expire as well.
 wait_row_count mac_binding 0 ip="192.168.10.10" logical_port="lr-ls1"
+wait_row_count mac_binding 0 ip="192.168.30.10" logical_port="lr-ls3"
 
 OVN_CLEANUP([hv1])
 AT_CLEANUP